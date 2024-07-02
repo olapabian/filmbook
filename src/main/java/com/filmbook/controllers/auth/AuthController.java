c5f764d43fc6a81c142021ee6c6f8a28fe7439f5
Your Name <you@example.com>
2024-07-02 20:19:11 +0200

    logout fix

--- src/main/java/com/filmbook/controllers/auth/AuthController.java
+++ src/main/java/com/filmbook/controllers/auth/AuthController.java
@@ -1,15 +1,18 @@
 package com.filmbook.controllers.auth;
 
+import com.filmbook.config.JwtUtil;
 import com.filmbook.config.UserAuthProvider;
 import com.filmbook.model.dto.CredentialsDto;
 import com.filmbook.model.dto.SignUpDto;
 import com.filmbook.model.dto.UserDto;
+import com.filmbook.services.auth.TokenService;
 import com.filmbook.services.auth.UserService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.net.URI;
@@ -19,11 +22,16 @@ public class AuthController {
 
     private final UserService userService;
     private final UserAuthProvider userAuthenticationProvider;
+    private final TokenService tokenService;
+    private final JwtUtil jwtUtil;
+
 
     @Autowired
-    public AuthController(UserService userService, UserAuthProvider userAuthenticationProvider) {
+    public AuthController(UserService userService, UserAuthProvider userAuthenticationProvider, TokenService tokenService, JwtUtil jwtUtil) {
         this.userService = userService;
         this.userAuthenticationProvider = userAuthenticationProvider;
+        this.tokenService = tokenService;
+        this.jwtUtil = jwtUtil;
     }
 
     @PostMapping("/login")
@@ -43,4 +51,12 @@ public class AuthController {
         return ResponseEntity.created(URI.create("/users/" + createdUser.getUserId())).body(createdUser);
     }
 
+    @PostMapping("/logoutUser")
+    public ResponseEntity<Void> logout(@RequestHeader("Authorization") String token) {
+        String tokenWithoutBearer = token.substring(7);
+        if (jwtUtil.validateToken(tokenWithoutBearer)) {
+            tokenService.invalidateToken(tokenWithoutBearer);
+        }
+        return ResponseEntity.noContent().build();
+    }
 }
